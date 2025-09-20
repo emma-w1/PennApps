ba6f7d96eaa461c6dcafa459990c6b74017f0864
Adishree Das <dasadishree@gmail.com>
2025-09-19 21:08:02 -0400

    start xcode project

---
+++ pennapps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pennapps
+//
+//  Created by Adishree Das on 9/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pennapps/pennappsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pennappsApp.swift
+//  pennapps
+//
+//  Created by Adishree Das on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct pennappsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
